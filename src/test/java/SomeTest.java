839f3086a2378d9a342935c0ddec3a006c0790e1
Artem <somemail2344@gmail.com>
2018-09-06 17:59:34 +0300

    Project created

---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class SomeTest {
+
+    @Test
+    public void TestCase1(){
+        System.out.println("TestCase1"); }
+
+    @Test
+    public void TestCase2(){
+        System.out.println("TestCase2"); }
+}
